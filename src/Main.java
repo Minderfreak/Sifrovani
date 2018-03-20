c6f49dc1b44c6587a5b3156616292344ccf3ac8c
matajzik <matajzik@PC541.SPSEJECNANET.CZ>
2018-03-20 11:08:04 +0100

    Uprava Main + Okno

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		try {
-		System.out.println(Sifrovani.zasifrovat("M:\\novej text.txt", "Vojta"));
-		}catch(Exception e) {
-			System.err.println(e.getMessage());
-		}
+		Okno o = new Okno();
 	}
 
 }
--- src/Okno.java
+++ src/Okno.java
@@ -1,4 +1,40 @@
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Okno {
+import javax.swing.*;
 
+public class Okno  extends JFrame{
+	private JLabel labelAdresa;
+	private JTextField fieldAdresa;
+	private JLabel labelHeslo;
+	private JTextField fieldHeslo;
+	private JButton button;
+	
+	public Okno() {
+		setTitle("ŠifrovaèkaTvojeMaèka");
+		setSize(500,100);
+		setLayout(new FlowLayout());
+		labelAdresa = new JLabel("Zadej adresu: ");
+		add(labelAdresa);
+		fieldAdresa = new JTextField(10);
+		add(fieldAdresa);
+		labelHeslo = new JLabel("Zadej heslo: ");
+		add(labelHeslo);
+		fieldHeslo = new JTextField(10);
+		add(fieldHeslo);
+		button = new JButton("Odkrágluj");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					System.out.println(Sifrovani.zasifrovat(labelAdresa.getText(), labelHeslo.getText()));
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		add(button);
+		setVisible(true);
+	}
 }
