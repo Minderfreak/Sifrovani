8e240b688a51f25153fd1c59925bb881079eac6a
muron <muron@PC540.SPSEJECNANET.CZ>
2018-03-20 10:57:36 +0100

    Tridy

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		try {
+		System.out.println(Sifrovani.zasifrovat("M:\\novej text.txt", "Vojta"));
+		}catch(Exception e) {
+			System.err.println(e.getMessage());
+		}
 	}
 
 }
--- src/Sifrovani.java
+++ src/Sifrovani.java
@@ -1,4 +1,23 @@
+import java.io.FileReader;
 
+/**
+ * Tøída pro šifrování
+ * @author Karel Muroò
+ *
+ */
 public class Sifrovani {
-
+	public static String zasifrovat(String adresa,String heslo) throws Exception {	
+		String vysledek="";
+		int mezi=0;
+		int index=0;
+		FileReader reader= new FileReader(adresa);
+		while((mezi=reader.read())!=-1) {
+			mezi= mezi ^ heslo.charAt(index);
+			vysledek+=(char)mezi;
+			index++;
+			if(index==heslo.length()) index=0;
+		}	
+		reader.close();
+		return vysledek;
+	}
 }
