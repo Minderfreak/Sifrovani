4e27a9f4c9272bea2ebc5fb56662704268675a4c
matajzik <matajzik@PC541.SPSEJECNANET.CZ>
2018-03-20 11:38:04 +0100

    Ochrana, aby tam byly čitelné znaky

--- src/Sifrovani.java
+++ src/Sifrovani.java
@@ -1,4 +1,5 @@
 import java.io.FileReader;
+import java.util.Random;
 
 /**
  * Tøída pro šifrování
@@ -6,6 +7,7 @@ import java.io.FileReader;
  *
  */
 public class Sifrovani {
+	public static Random r = new Random();
 	public static String zasifrovat(String adresa,String heslo) throws Exception {	
 		String vysledek="";
 		int mezi=0;
@@ -13,6 +15,12 @@ public class Sifrovani {
 		FileReader reader= new FileReader(adresa);
 		while((mezi=reader.read())!=-1) {
 			mezi= mezi ^ heslo.charAt(index);
+			/*while(mezi<33) {
+				mezi+=r.nextInt(60);
+			}
+			while(mezi>126) {
+				mezi-=r.nextInt(50);
+			}*/
 			vysledek+=(char)mezi;
 			index++;
 			if(index==heslo.length()) index=0;
